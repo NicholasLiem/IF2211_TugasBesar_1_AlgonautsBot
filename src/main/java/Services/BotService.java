93f7fb9ce1e75adf1856991414d6c9de3bbd5c3f
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-02-13 00:05:34 +0700

    feat: adding defense mechanism

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -2,6 +2,7 @@ package Services;
 
 import Enums.*;
 import Models.*;
+import java.lang.Math;
 
 import java.io.Console;
 import java.util.*;
@@ -15,6 +16,7 @@ public class BotService {
     private GameObject target;
     private GameObject worldCenter;
     private Boolean attacking;
+    private Boolean targeted;
     private PlayerAction playerAction;
     private GameState gameState;
     private Boolean abON;
@@ -28,6 +30,7 @@ public class BotService {
         position.setY(0);
         this.worldCenter = new GameObject(null, null, null, null, position, null, null, null, null, null, null);
         this.abON = false;
+        this.targeted = false;
     }
 
     public GameObject getBot() {
@@ -51,7 +54,7 @@ public class BotService {
         playerAction.heading = new Random().nextInt(360);
 
         if (target == null || target == worldCenter) {
-            playerAction.heading = findingNewTarget();
+            playerAction.heading = findingNewTarget();  
         } else {
             // decide if new target is an object or a player
             if (!gameState.getGameObjects().stream().filter(item -> item.id == target.id).collect(Collectors.toList())
@@ -62,6 +65,7 @@ public class BotService {
                 if (target.getSize() < bot.size) {
                     playerAction.heading = getHeadingBetween(target);
                 } else {
+                    targeted = true;
                     playerAction.heading = findingNewTarget();
                 }
             } else if (!gameState.getPlayerGameObjects().stream().filter(item -> item.id == target.id)
@@ -72,6 +76,7 @@ public class BotService {
                 if (target.getSize() < bot.size) {
                     playerAction.heading = getHeadingBetween(target);
                 } else {
+                    targeted = true;
                     playerAction.heading = findingNewTarget();
                 }
             } else {
@@ -89,11 +94,22 @@ public class BotService {
         if (attacking) {
             System.out.println("ATTACKING");
         }
-        if (attacking && !abON && bot.size > 50) {
+        // if (!attacking) {
+            //     System.out.println("ATTACKING IS FALSE");
+            // }
+
+        // Torpedo dateng
+        // if (targeted) {
+        //     playerAction.action = PlayerActions.ACTIVATESHIELD;
+        //     System.out.println("ACTIVATING SHIELD");
+        // }
+
+        // Kasih tick buat afterburner
+        if (attacking && !abON && bot.size > target.getSize() + 20) {
             playerAction.action = PlayerActions.STARTAFTERBURNER;
             abON = true;
             System.out.println("AfterBurner On");
-        } else if ((!attacking || bot.size < 45) && abON) {
+        } else if ((!attacking || bot.size < 45 || bot.size < target.getSize() + 10) && abON) {
             playerAction.action = PlayerActions.STOPAFTERBURNER;
             abON = false;
             System.out.println("AfterBurner Off");
@@ -101,49 +117,7 @@ public class BotService {
             playerAction.action = PlayerActions.FIRETORPEDOES;
             System.out.println("Firing Torpedo");
         }
-        // *********************** BORDER ********************************** //
-        // cek ada player atau gak di sebelahnya
-        // if (!gameState.getGameObjects().isEmpty()) {
-        // // if the player is all nearby sort the player by the smallest size of player
-        // so you can eat the player
-        // var playerList = gameState.getPlayerGameObjects().stream().filter(item ->
-        // item.getId() != bot.id && item.getGameObjectType() == ObjectTypes.PLAYER)
-        // .sorted(Comparator.comparing(item -> item.getSize()))
-        // .collect(Collectors.toList());
-        // // di sini udah ada list of food dengan distance food dengan player terurut
-        // menaik dari terkecil hingga terbesar
-        // var foodList = gameState.getGameObjects()
-        // .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
-        // .sorted(Comparator
-        // .comparing(item -> getDistanceBetween(bot, item)))
-        // .collect(Collectors.toList());
-        // if (bot.getSize() + 10 > playerList.get(0).getSize()) {
-        // // action
-        // // defense if player size is bigger than this player (menghindar)
-        // // attack if bigger
-        // playerAction.heading = getHeadingBetween(playerList.get(0));
-        // } else {
-        // playerAction.heading = getHeadingBetween(foodList.get(0));
-        // }
-
-        // // else if (foodList.size() > 0) {
-        // // // action : getting food by the smallest distance
-        // // playerAction.heading = getHeadingBetween(foodList.get(0));
-        // // }
-        // // syntax masih salah
-        // }
-        // else if (!gameState.getPlayerGameObjects().isEmpty()) {
-        // // what time can we attack?
-        // // how to check if this player has any weapon? search from the game state?
-        // what kind of weapon and what kind of priority weapon do we have to arrange?
-        // // arrange the weapon in the set by its priority (make the array to store
-        // weapon by priority?)
-        // // fire the first priority weapon
-        // // action : attack anytime you can
-        // }
-
         this.playerAction = playerAction;
-
     }
 
     private int findingNewTarget() {
@@ -154,46 +128,82 @@ public class BotService {
                     .filter(item -> item.getGameObjectType() == ObjectTypes.PLAYER && item.getId() != bot.id)
                     .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item)))
                     .collect(Collectors.toList());
-
-            // getting list of foods and sorting it by distance terkecil ke terbesar
             var foodList = gameState.getGameObjects()
                     .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
                     .sorted(Comparator
-                            .comparing(item -> getDistanceBetween(bot, item)))
+                    .comparing(item -> getDistanceBetween(bot, item)))
                     .collect(Collectors.toList());
+            var gasCloudsList = gameState.getGameObjects()
+                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.GASCLOUD)
+                    .sorted(Comparator
+                    .comparing(item -> getDistanceBetween(bot, item)))
+                    .collect(Collectors.toList());
+            var wormHoleList = gameState.getGameObjects()
+                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.WORMHOLE)
+                    .sorted(Comparator
+                    .comparing(item -> getDistanceBetween(bot, item)))
+                    .collect(Collectors.toList());
+            
+            // var nearestWormhole = wormHoleList.get(0);
+            
+            if (playerList.isEmpty()){
+                System.out.println("YEY MENANG");
+            }
             var nearestPlayer = playerList.get(0);
             var nearestFood = foodList.get(0);
-            // getting direction to nearest player of nearest food
+            var nearestGasCloud = gasCloudsList.get(0);
+
             var headsToNearestPlayer = getHeadingBetween(nearestPlayer);
             var headsToNearestFood = getHeadingBetween(nearestFood);
-
+            // var distanceFromFoodToGasClouds = getDistanceBetween(nearestFood, nearestGasCloud);
+            
             if (nearestPlayer.getSize() > bot.size) {
-                // in this case the nearest player is a threat because it's bigger
-                heading = headsFarFromAttacker(nearestPlayer, nearestFood);
-                attacking = false;
+                heading = headsFarFromAttacker(nearestPlayer, nearestFood, wormHoleList);
+                this.attacking = false;
+                this.targeted = true;
+                System.out.println("DIKEJAR MUSUH");
             } else if (nearestPlayer.getSize() < bot.size) {
-                // in this case the nearest player is a prey
                 heading = headsToNearestPlayer;
-                target = nearestPlayer;
-                attacking = true;
+                this.target = nearestPlayer;
+                this.attacking = true;
+                this.targeted = false;
+                System.out.println("NGEJAR MUSUH");
             } else if (nearestFood != null) {
-                // in this case the nearest player has the same size as our bot, so we chase for
-                // foods
+                this.targeted = false;
                 heading = headsToNearestFood;
-                target = nearestFood;
-                attacking = false;
+                this.target = nearestFood;
+                this.attacking = false;
+                System.out.println("NYARI MAKAN");
             } else {
                 // in this case we are in empty area where there is no food or enemy, high
                 // chance near border so we go to center
-                target = worldCenter;
+                this.targeted = false;
+                this.target = worldCenter;
                 heading = getHeadingBetween(worldCenter);
-                attacking = false;
-
-            }
-            if (target == worldCenter) {
-                heading = headsToNearestPlayer;
+                this.attacking = false;
+                System.out.println("KE TENGAH?");
             }
 
+            if (target != null) {
+            var distanceTargetFromGasClouds = getDistanceBetween(nearestGasCloud, target);
+                if (distanceTargetFromGasClouds < 10 &&
+                    Math.abs(getHeadingBetween(target) - getHeadingBetween(nearestGasCloud)) < 45) {
+                    heading = headsInverse(nearestGasCloud);
+                    System.out.println("AWAY FROM GAS CLOUD");
+                }
+                if (target == worldCenter) {
+                    if (attacking){
+                        heading = headsToNearestPlayer;
+                    } else {
+                        if (targeted) {
+                            heading = headsFarFromAttacker(nearestPlayer, nearestFood, wormHoleList);
+                        } else {
+                            heading = getHeadingBetween(worldCenter);
+                        }
+                    }
+                return heading;
+                }
+            }
             return heading;
         }
         return getHeadingBetween(worldCenter);
@@ -201,28 +211,65 @@ public class BotService {
 
     // calculating the direction we're heading to when there is enemy bigger and
     // near us
-    private int headsFarFromAttacker(GameObject enemy, GameObject nearestFood) {
-        // nearest food null berarti gak bisa escape pake food equals to running pake
-        // opposite direction
+    private int headsFarFromAttacker(GameObject enemy, GameObject nearestFood, List<GameObject> wormHoleList) {
         int heading;
         if (nearestFood == null) {
             heading = headsInverse(enemy);
         }
 
+        GameObject nearestWormHole;
+        if (!wormHoleList.isEmpty()) {
+            nearestWormHole = wormHoleList.get(0);
+        } else {
+            nearestWormHole = null;
+        }
+
         var distanceFromEnemy = getDistanceBetween(bot, enemy);
-        // check distance between the food and attacker so we can chase the food that is
-        // far from the enemy
         var foodAndEnemyDistance = getDistanceBetween(nearestFood, enemy);
+        var wormHoleAndEnemyDistance = getDistanceBetween(nearestWormHole, enemy);
 
         // parameternya adalah ketika speed enemy lebih kecil daripada distance
         // artinya si enemy gak dalam range yang bisa ngejar kita dengan cepat
         // dan ketika distance food-nya sangat jauh dari attacker, kita bisa makan
         // dengan menjauh secara gak langsung dari enemy yang lebih besar itu
-        if (distanceFromEnemy > enemy.speed && foodAndEnemyDistance > distanceFromEnemy) {
+        
+        /* Kita akan melakukan "EATING" ketika:
+            1. Jarak Dengan Musuh > Kecepatan Musuh
+            2. Jarak Makanan dan Musuh > Jarak Bot dgn Musuh
+            3. Jarak Wormhole dan Musuh > Jarak Bot dgn Musuh
+            4. Jarak Makanan dan Musuh < Jarak Wormhole dan Musuh
+        */
+
+        /* 
+            foodAndEnemyDistance > distanceFromEnemy && 
+            wormHoleAndEnemyDistance > distanceFromEnemy && 
+            foodAndEnemyDistance < wormHoleAndEnemyDistance &&
+        */
+
+        if (distanceFromEnemy > enemy.speed &&  
+            enemy.size > bot.size && 
+            foodAndEnemyDistance > distanceFromEnemy + 5) {
             heading = getHeadingBetween(nearestFood);
+            System.out.println("EATING");
+
+        /* Kita akan melakukan pergi ke Wormhole ketika:
+            1. Jarak Dengan Musuh > Kecepatan Musuh
+            2. Jarak Makanan dan Musuh > Jarak Bot dgn Musuh
+            3. Jarak Wormhole dan Musuh > Jarak Bot dgn Musuh
+            4. Jarak Makanan dan Musuh > Jarak Wormhole dan Musuh
+        */
+        /* 
+            foodAndEnemyDistance > distanceFromEnemy && 
+            wormHoleAndEnemyDistance > distanceFromEnemy && 
+            foodAndEnemyDistance > wormHoleAndEnemyDistance
+        */
+        
+        } else if (wormHoleAndEnemyDistance > distanceFromEnemy && nearestWormHole != null) {
+            heading = getHeadingBetween(nearestWormHole);
+            System.out.println("GOING TO WORMHOLE");
         } else {
             heading = headsInverse(enemy);
-            System.out.println("GOING AWAY FROM ATTACKER");
+            System.out.println("GOING AWAY FROM ATTACKER");   
         }
         return heading;
     }
@@ -247,6 +294,10 @@ public class BotService {
     }
 
     private double getDistanceBetween(GameObject object1, GameObject object2) {
+        if (object1 == null || object2 == null){
+            return 0;
+        }
+
         var triangleX = Math.abs(object1.getPosition().x - object2.getPosition().x);
         var triangleY = Math.abs(object1.getPosition().y - object2.getPosition().y);
         return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
